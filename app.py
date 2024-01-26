8863d6d6aac7c1a7027be7a985f6f1f2d563715a
Rahuldev Sarkar <email.rahuldev@gmail.com>
2024-01-27 00:48:22 +0530

    Minor adjustments

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from constants.common_constants import SECRET_KEY, DATABASE_URI, TRACK_MODIFICATION
+from constants.common_constants import (
+    APPLICATION_SECRET_KEY,
+    DATABASE_URI,
+    SECRET_KEY,
+    SQLITE_DATABASE_FILE,
+    TRACK_MODIFICATION,
+)
 
 app = Flask(__name__)
-app.config[SECRET_KEY] = "-fA2915AGHcgjROd4cym1meUYEI"
-app.config[DATABASE_URI] = "sqlite:///myappdb.db"
+app.config[SECRET_KEY] = APPLICATION_SECRET_KEY
+app.config[DATABASE_URI] = SQLITE_DATABASE_FILE
 app.config[TRACK_MODIFICATION] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -14,4 +20,4 @@ migrate = Migrate(app, db)
 if __name__ == "__main__":
     from views import *
 
-    app.run(debug=True)
+    app.run(debug=True)  # ssl_context="adhoc" will enable https for test
